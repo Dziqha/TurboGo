4926c42b495ac8cc6a82876861c9edda8b2ead7f
Dziqha <lulussnbp01@gmail.com>
2025-07-06 19:25:09 +0700

    chore: remove unused code and update .gitignore

--- internal/cache/engine.go
+++ internal/cache/engine.go
@@ -2,17 +2,10 @@ package cache
 
 type Engine struct {
 	Memory  *InMemCache  // from inmem.go
-	Storage *PersistentCache  // from persist.go
 }
 
 func NewEngine() (*Engine, error) {
-	persist, err := NewPersistent("data/cache.json", false)
-	if err != nil {
-		return nil, err
-	}
-
 	return &Engine{
 		Memory:  NewInMem(),
-		Storage: persist,
 	}, nil
 }
